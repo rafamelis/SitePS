c575bb10e20ac27bd88e804e0063aabbe86e2a15
Rafamelis <rafael_melis@hotmail.com>
2023-04-18 16:27:45 -0300

    Att

--- style-script/script.js
+++ style-script/script.js
@@ -1,2 +1,10 @@
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
+
+const elementH1 = document.querySelector ('section h1');
+const container = document.querySelector ('.container');
+elementH1.addEventListener('click', () =>{
+
+    container.classList.toggle('active')
+
+})
